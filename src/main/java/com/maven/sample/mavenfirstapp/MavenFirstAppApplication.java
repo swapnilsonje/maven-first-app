f8ebcda6a94b5fd941b24dd52923b2c9d4dbdc17
s.sonje@maxxton.com <s.sonje@maxxton.com>
2021-04-19 00:44:13 +0530

    whole project

---
+++ src/main/java/com/maven/sample/mavenfirstapp/MavenFirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.maven.sample.mavenfirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenFirstAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maven/sample/mavenfirstapp/MavenFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maven.sample.mavenfirstapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
